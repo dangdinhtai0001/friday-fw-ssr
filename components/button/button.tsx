afe9e49aaf5266e125bdb6d56a7c369cdac8be09
dang dinh tai <tai.dang@fssc.com.vn>
2022-10-26 17:41:04 +0700

    add next/themes

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,8 +1,8 @@
 import { createButtonClassname } from "./helper";
 
 const Button = (props: any) => {
-  const { children } = props;
-  return <button className={createButtonClassname(props)}>{children}</button>;
+  const { children, onClick } = props;
+  return <button className={createButtonClassname(props)} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
--- components/button/helper.ts
+++ components/button/helper.ts
@@ -2,7 +2,15 @@ const DEFAULT_PADDING = "px-1 py-1";
 const DEFAULT_CLASSNAME = DEFAULT_PADDING + "";
 
 const createButtonClassname = (props: any) => {
-  const { pill, outline, bordered, disabled, elevated, color } = props;
+  const {
+    pill,
+    outline,
+    bordered = true,
+    disabled,
+    elevated,
+    color,
+    className = "",
+  } = props;
 
   let _className = DEFAULT_CLASSNAME;
   let _color = color;
@@ -13,11 +21,13 @@ const createButtonClassname = (props: any) => {
   _className += ` bg-${_color ? _color : "primary"}`;
   _className += ` ${pill ? "rounded-full" : "rounded"}`;
   _className += ` ${outline ? "bg-transparent border" : ""}`;
-  _className += ` ${bordered ? "border" : ""}`;
+  _className += ` ${bordered ? "border-1 border-black" : ""}`;
   _className += ` ${disabled ? "opacity-50 cursor-not-allowed" : ""}`;
   _className += ` ${elevated ? "shadow" : ""}`;
 
-  return _className;
+  console.log(_color);
+
+  return `${_className} ${className}`;
 };
 
 export { createButtonClassname };
