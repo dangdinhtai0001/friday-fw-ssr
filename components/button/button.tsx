693fffb1d4935a0b66e014e611ae945f1349dfe6
dang dinh tai <tai.dang@fssc.com.vn>
2022-10-27 11:23:12 +0700

    fix lỗi không nhận css component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,10 +1,15 @@
+// import './button.module.scss';
+import styles from './button.module.scss'
+
 const Button = () => {
   return (
     <button
       type="button"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      // Note how the "error" class is accessed as a property on the imported
+      // `styles` object.
+      className={styles.error}
     >
-      Default
+      Destroy
     </button>
   );
 };
