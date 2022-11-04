665f4430fd7abe6167de5bfd091e7f1aa4bc15e5
dang dinh tai <tai.dang@fssc.com.vn>
2022-11-04 14:11:42 +0700

    clean branch

--- components/portal/Context.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export type QueueCreate = (appendFunc: VoidFunction) => void;
-
-const OrderContext = React.createContext<QueueCreate>(() => { });
-
-export default OrderContext;
