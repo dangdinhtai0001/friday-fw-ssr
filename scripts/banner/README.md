ce0a100b92fb52b111692fead43cc8e0fde613c1
dinhtai@ubuntu <dangdinhtai0001>
2022-11-29 10:15:54 +0700

    feat: thêm script auto generate new component cho linux

---
+++ scripts/banner/README.md
@@ -0,0 +1,7 @@
+nơi lưu các file ascii để làm banner cho các script (cho đẹp thôi =)))) )
+
+_một số nguồn_
+
+- [emojicombos](https://emojicombos.com/gryffindor-ascii-art)
+
+_một số art_
--- scripts/generate/generate.sh
+++ scripts/generate/generate.sh
@@ -1,15 +1,55 @@
 #! /bin/bash
 
-COMPONENT_NAME='Portal'
-
-echo "Change working dir"
 cd "$(dirname "$0")"
-pwd
+cat ../banner/script-banner.txt
+echo 
+echo 
+# -----------------------------
+echo "Enter the component name"
+read COMPONENT_NAME
 
-echo "cp $COMPONENT_NAME"
-cp -r components ../../packages/$COMPONENT_NAME
+echo "Choose package"
+echo "1- gryffindor"
+echo "2- slytherin"
+echo "3- ravenclaw"
+echo "4- hufflepuff"
 
-mv ../../packages/$COMPONENT_NAME/BaseComponent.tsx ../../packages/$COMPONENT_NAME/$COMPONENT_NAME.tsx
+read PACKAGE_CODE
 
-echo "replace"
-sed 's/BaseComponent/'$COMPONENT_NAME'/g' ../../packages/$COMPONENT_NAME/$COMPONENT_NAME.tsx
+PACKAGE_NAME=''
+case $PACKAGE_CODE in
+    1)
+        cat ../banner/gryffindor.txt
+        PACKAGE_NAME='gryffindor'
+        ;;  
+    2)
+        cat ../banner/slytherin.txt
+        PACKAGE_NAME='slytherin'
+        ;; 
+    3)
+        cat ../banner/ravenclaw.txt
+        PACKAGE_NAME='ravenclaw'
+        ;;  
+    4)
+        cat ../banner/hufflepuff.txt
+        PACKAGE_NAME='hufflepuff'
+        ;;
+
+    *)
+    ;;
+esac
+
+COMPONENT_DIR='../../packages/'$PACKAGE_NAME/$COMPONENT_NAME
+
+echo
+echo "Start generate component $COMPONENT_NAME in $COMPONENT_DIR"
+
+
+
+mkdir $COMPONENT_DIR
+
+sed 's/BaseComponent/'$COMPONENT_NAME'/g' components/BaseComponent.tsx > $COMPONENT_DIR/$COMPONENT_NAME.tsx
+sed 's/BaseComponent/'$COMPONENT_NAME'/g' components/index.ts > $COMPONENT_DIR/index.ts
+sed 's/BaseComponent/'$COMPONENT_NAME'/g' components/interfaces.ts > $COMPONENT_DIR/interfaces.tsx
+
+echo "Done!!!"
