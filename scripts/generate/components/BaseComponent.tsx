72597c4e745e93d39471d36fa8110c564ed40c67
dinhtai@ubuntu <dangdinhtai0001>
2022-11-30 14:23:29 +0700

    refactor: cập nhật lại generator template

--- scripts/generate/components/BaseComponent.tsx
+++ scripts/generate/components/BaseComponent.tsx
@@ -1,10 +1,11 @@
 // react imports
+import * as React from 'react';
 // 3rd imports
 // local imports
 import { BaseComponentProps } from './BaseComponent.d';
 
-const BaseComponent: React.FC<BaseComponentProps> = () => {
-    return <div></div>;
-};
+const BaseComponent: React.FC<BaseComponentProps> = React.forwardRef((props: BaseComponentProps, ref: React.ForwardedRef<unknown>) => {
+    return <></>;
+});
 
 export default BaseComponent;
