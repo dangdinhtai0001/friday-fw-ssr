8a9a291f6c08f82bef3712f1e6b8d784b58c9a14
dinhtai@ubuntu <dangdinhtai0001>
2022-11-30 11:57:37 +0700

    refactor: sử dụng @MUI/utils và @MUI/base

---
+++ modules/demo/DemoPage.tsx
@@ -0,0 +1,13 @@
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+
+const DemoPage = () => {
+    return (
+        <>
+            DemoPage
+            <div>
+                <ButtonUnstyled>Hello World</ButtonUnstyled>
+            </div>
+        </>);
+}
+
+export default DemoPage;
---
+++ pages/demo/index.tsx
@@ -0,0 +1,6 @@
+import { DemoPage } from '@modules/demo';
+const NextPage = () => {
+    return <DemoPage />;
+}
+
+export default NextPage;
