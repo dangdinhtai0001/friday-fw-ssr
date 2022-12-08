f5c04912779736d1ead04529b8718c75dff7b670
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2022-12-08 22:50:05 +0700

    feat: cấu hình theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <div>Hello</div>;
-}
+import type { NextPage } from 'next';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+
+const themes = [{ name: 'Light' }, { name: 'Dark' }, { name: 'Emerald' }, { name: 'Pink' }];
+
+const Home: NextPage = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className="bg-th-background w-full h-screen">
+      <div className="py-20 flex flex-col gap-3 items-center justify-center">
+        <h1 className="text-5xl text-center  font-bold text-th-primary">
+          Next Themes + Tailwind Dark Mode
+        </h1>
+        <select
+          name="theme"
+          id="theme-select"
+          className="bg-white text-gray-800 border-gray-800 border py-1 px-3"
+          onChange={e => setTheme(e.currentTarget.value)}
+          value={theme}
+        >
+          <option value="">Select Theme</option>
+          {themes.map(t => (
+            <option key={t.name.toLowerCase()} value={t.name.toLowerCase()}>
+              {t.name}
+            </option>
+          ))}
+        </select>
+
+        <Link href="/demo">demo</Link>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './modules/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'th-background': 'var(--background)',
+        'th-foreground': 'var(--foreground)',
+        // -----------------------------
+        'th-primary': 'var(--primary)',
+        'th-secondary': 'var(--secondary)',
+        'th-success': 'var(--success)',
+        'th-danger': 'var(--danger)',
+        'th-warning': 'var(--warning)',
+        'th-info': 'var(--info)',
+        // -----------------------------
+        'th-text-primary': 'var(--text-primary)',
+      },
+    },
   },
   plugins: [],
 };
