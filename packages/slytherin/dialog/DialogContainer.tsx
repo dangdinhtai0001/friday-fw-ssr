1b6fca5f83c3250eb5e95451c9894594466cc23b
dinhtai@ubuntu <dangdinhtai0001>
2022-12-09 17:44:04 +0700

    feat: thêm tính năng resize

--- packages/slytherin/dialog/DialogContainer.tsx
+++ packages/slytherin/dialog/DialogContainer.tsx
@@ -1,7 +1,7 @@
 // react imports
 import * as React from 'react';
 // 3rd imports
-import { AnimatePresence, motion, useAnimation } from 'framer-motion';
+import { AnimatePresence, motion, PanInfo, useAnimation, useMotionValue } from 'framer-motion';
 // local imports
 import { DialogContainerProps } from './Dialog.d';
 import { useDialogContext } from './DialogContext';
@@ -30,13 +30,25 @@ const variants = {
         },
     },
 };
+
 function Container(props: DialogContainerProps, ref: React.ForwardedRef<any>): JSX.Element {
     const { children } = props;
 
     const { context } = useDialogContext();
-    const controls = useAnimation();
 
+    const controls = useAnimation();
+    const mHeight = useMotionValue(200);
+
+    const [isResizing, setIsResizing] = React.useState(false);
     const constraintsRef = React.useRef(null);
+
+
+    const handleOnResize = React.useCallback((event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo, direction: 'top' | 'bottom' | 'left' | 'right') => {
+        let newHeight = mHeight.get() + info.delta.y;
+        if (newHeight > 200 && newHeight < 400) {
+            mHeight.set(mHeight.get() + info.delta.y);
+        }
+    }, []);
 
     React.useEffect(() => {
         controls.start("visible");
@@ -56,7 +68,39 @@ function Container(props: DialogContainerProps, ref: React.ForwardedRef<any>): J
                 key="fd--dialog-panel"
                 ref={constraintsRef}
             >
-                <motion.div drag dragConstraints={constraintsRef} className=' flex flex-col rounded-t-[0.5rem] rounded-b-[0.5rem] bg-th-background' >
+                <motion.div
+                    drag={!isResizing}
+                    dragConstraints={constraintsRef}
+                    className=' flex flex-col rounded-t-[0.5rem] rounded-b-[0.5rem] bg-th-background'
+                    style={{
+                        height: mHeight,
+                        width: 200,
+                        cursor: isResizing ? "row-resize" : "",
+                    }}
+                >
+                    {/* ========================================================= resize top =========================================================*/}
+                    <motion.div
+                        style={{
+                            cursor: "row-resize",
+                            textAlign: "center",
+                            userSelect: "none",
+                            width: '100%',
+                            height: '5px',
+                        }}
+                        className='bg-transparent'
+                        drag="y"
+                        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+                        dragElastic={0}
+                        dragMomentum={false}
+                        onDrag={(e, info) => { handleOnResize(e, info, 'top') }}
+                        onDragEnd={() => {
+                            setIsResizing(false);
+                        }}
+                        onDragStart={() => {
+                            setIsResizing(true);
+                        }}
+                    ></motion.div>
+                    {/* ========================================================= resize top =========================================================*/}
 
                     {/* ------------------ | header | ------------------ */}
                     <div id="__fd-dialog-title" className="flex items-center justify-center text-th-text-primary font-[600] text-[1.3rem] h-[2.1rem] w-full py-[1rem] bg-th-primary rounded-t-[0.5rem]">
@@ -70,6 +114,30 @@ function Container(props: DialogContainerProps, ref: React.ForwardedRef<any>): J
                     <div className="h-fit rounded-b-[0.5rem] border-t-[0.1rem] flex justify-end gap-[0.5rem] px-[0.5rem] py-[0.3rem]">
                         Đây là footer
                     </div>
+
+                    {/* ========================================================= resize bot =========================================================*/}
+                    <motion.div
+                        style={{
+                            cursor: "row-resize",
+                            textAlign: "center",
+                            userSelect: "none",
+                            width: '100%',
+                            height: '1px',
+                        }}
+                        drag="y"
+                        dragConstraints={{ top: -5, left: 0, right: 0, bottom: 0 }}
+                        dragElastic={0}
+                        dragMomentum={false}
+                        onDrag={(e, info) => { handleOnResize(e, info, 'bottom') }}
+                        onDragEnd={() => {
+                            setIsResizing(false);
+                        }}
+                        onDragStart={() => {
+                            setIsResizing(true);
+                        }}
+                    />
+                    {/* ========================================================= resize bot =========================================================*/}
+
                 </motion.div>
             </motion.div>
         </AnimatePresence>
