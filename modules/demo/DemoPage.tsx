c22bf5ab07752adc3bd17bcedddefadb5611ac57
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2023-03-02 00:23:30 +0700

    create new demo page

---
+++ modules/demo/DemoPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return <div>Hello World!</div>;
+};
+
+export default Page;
---
+++ pages/demo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DemoPage from '@/modules/demo/DemoPage'
+
+const Page: React.FC = () => {
+    return <DemoPage />;
+};
+
+export default Page;
