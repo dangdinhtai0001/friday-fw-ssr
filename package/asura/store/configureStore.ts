1a3581548aff5dd65d0441b3bf720ab8061682f6
dangdinhtai0001 <dangdinhtai0001@gmail.com>
2023-03-12 13:53:24 +0700

    feat: thêm middleware cho redux

--- package/asura/store/configureStore.ts
+++ package/asura/store/configureStore.ts
@@ -1,14 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 // slice import
 import themeReducer from './slice/themeSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,20 @@ import type { AppProps } from 'next/app';
 // 3rd import
 import { Provider } from 'react-redux';
 // local import;
-import { store } from '@/package/asura/store/configureStore';
-
+// import { store } from '@/package/asura/store/configureStore';
 import App from '@/package/asura/app/App';
+import {
+  persistor,
+  store,
+} from '@/package/asura/store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function NextApp(props: AppProps) {
   return (
     <Provider store={store}>
-      <App {...props} />
+      <PersistGate loading={null} persistor={persistor}>
+        <App {...props} />
+      </PersistGate>
     </Provider>
   );
 }
