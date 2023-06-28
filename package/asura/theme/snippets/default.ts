763e94f061b46d7a9574c034cf5e893325ece417
dinhtai@ubuntu <dangdinhtai0001>
2023-06-28 10:11:07 +0700

    feat: cấu hình thêm các option cho theme

---
+++ package/asura/theme/snippets/default.ts
@@ -0,0 +1,48 @@
+import { ThemeOptions } from '@mui/system';
+
+export const snippets: ThemeOptions = {
+  spacing: 8,
+  shape: {
+    borderRadius: 10,
+  },
+  components: {
+    MuiButton: {
+      size: 'small',
+    },
+    MuiButtonGroup: {
+      size: 'small',
+    },
+    MuiCheckbox: {
+      size: 'small',
+    },
+    MuiFab: {
+      size: 'small',
+    },
+    MuiFormControl: {
+      margin: 'dense',
+      size: 'small',
+    },
+    MuiFormHelperText: {
+      margin: 'dense',
+    },
+    MuiIconButton: {
+      size: 'small',
+    },
+    MuiInputBase: {
+      margin: 'dense',
+    },
+    MuiInputLabel: {
+      margin: 'dense',
+    },
+    MuiRadio: {
+      size: 'small',
+    },
+    MuiSwitch: {
+      size: 'small',
+    },
+    MuiTextField: {
+      margin: 'dense',
+      size: 'small',
+    },
+  },
+};
