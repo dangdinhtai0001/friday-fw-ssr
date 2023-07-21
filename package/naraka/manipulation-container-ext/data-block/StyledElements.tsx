5bbf679e4a45dbc43074093eb5ebf76c807951e0
dinhtai@ubuntu <dangdinhtai0001>
2023-07-21 09:28:34 +0700

    feat: define các shared style

--- package/naraka/manipulation-container-ext/data-block/StyledElements.tsx
+++ package/naraka/manipulation-container-ext/data-block/StyledElements.tsx
@@ -1,6 +1,6 @@
 import styled from '@mui/system/styled';
 import { IDefaultTheme } from '@/package/preta/types';
-import { typographyBody1Strong } from '@/package/preta/styled-shared';
+import { typographyCaption1Strong } from '@/package/preta/styled-shared';
 import {
   IStyledLabelProps,
   IStyledMessageProps,
@@ -8,7 +8,7 @@ import {
 } from './types.d'
 
 export const StyledLabel = styled('div', {})<IStyledLabelProps>(({ theme, status, textAlign }: IStyledLabelProps) => ({
-  ...typographyBody1Strong({ theme }),
+  ...typographyCaption1Strong({ theme }),
 
   color: status === 'error' ? theme?.palette.error.main :
     status === 'warning' ? theme?.palette.warning.main : 'inherit',
