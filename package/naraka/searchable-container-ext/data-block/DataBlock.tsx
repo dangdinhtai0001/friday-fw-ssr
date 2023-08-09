941e1a29dd483364d7e4c8f6cc01bb8382fd9451
dinhtai@ubuntu <dangdinhtai0001>
2023-08-09 18:00:36 +0700

    feat: add ag grid

--- package/naraka/searchable-container-ext/data-block/DataBlock.tsx
+++ package/naraka/searchable-container-ext/data-block/DataBlock.tsx
@@ -3,6 +3,10 @@ import { IDataBlockExtProps } from './types.d';
 import { useContainerContext } from '@/package/naraka/searchable-container';
 import { ContextHookValue } from '@/package/naraka/searchable-container/types';
 
+import { AgGridReact } from 'ag-grid-react';
+// import 'ag-grid-community/dist/styles/ag-grid.css';
+// import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 function DataBlock(props: IDataBlockExtProps) {
   const { context, contextApi }: ContextHookValue = useContainerContext();
   const { onCreateTaskChain } = props;
