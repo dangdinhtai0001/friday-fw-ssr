63854e1267f66f9138eeb0bd5530ba8af0398050
dangdinhtai <dangdinhtai0001@gmail.com>
2023-10-12 16:29:31 +0700

    stles: Thêm cấu hình measurement (déign token)

--- src/package/michael/theme/sematic-tokens/index.ts
+++ src/package/michael/theme/sematic-tokens/index.ts
@@ -1,5 +1,10 @@
 import { colorToken } from './colors';
+import { measurement } from './measurement';
 
 export const semanticTokens = {
-    colors: colorToken
+    colors: colorToken,
+
+    space: { measurement: measurement },
+    borderRadius: { measurement: measurement },
+    size: { measurement: measurement }
 };
