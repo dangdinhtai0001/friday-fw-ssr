c640831709e8f31bb992c394bbf6eab90b103f59
dangdinhtai <dangdinhtai0001@gmail.com>
2023-10-23 18:00:05 +0700

    refactor:

--- src/package/gabriel/admin/components/base/Page.tsx
+++ src/package/gabriel/admin/components/base/Page.tsx
@@ -1,7 +1,12 @@
+import { Box, Text, Divider } from "@chakra-ui/react";
+
 export default function Page() {
     return (
-        <div>
-            Component base nè
-        </div>
+        <>
+            <Box p='measurement.40'>
+                <Text color='black.100' textStyle='48.semibold'>Base components</Text>
+            </Box>
+            <Divider />
+        </>
     );
 }
